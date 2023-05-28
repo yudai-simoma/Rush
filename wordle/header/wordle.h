82d5600f7b1c292981d1cbd64cf6687143287f1a
yuda-simoma <cdcdasdll@icloud.com>
2023-05-28 19:32:26 +0900

    program完成

--- wordle/header/wordle.h
+++ wordle/header/wordle.h
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 11:15:10 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/05/27 21:38:33 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:30:24 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,21 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <time.h>
+# include <ctype.h>
 # include "get_next_line_bonus.h"
 
 # define RED_COLOR "\033[31m"
+# define GREEN_COLOR "\033[32m"
 # define YELLOW_COLOR "\033[33m"
 # define GREY_COLOR "\033[90m"
 # define RESET_COLOR "\033[0m"
 # define FILE_NAME "./files/words.txt"
 # define TRY_COUNT 6
 # define PRINT_COUNT 5
-# define RED_FLG 0
-# define YELLOW_FLG 1
-# define GREY_FLG 2
-# define NO_COLOR_FLG 3
+# define GREEN_FLG 1
+# define YELLOW_FLG 2
+# define GREY_FLG 3
+# define NO_COLOR_FLG 4
 
 typedef struct s_wordle
 {
@@ -40,7 +42,13 @@ typedef struct s_wordle
 	size_t dictionary_size;
 	char put_str[TRY_COUNT][PRINT_COUNT];
 	int color_info[TRY_COUNT][PRINT_COUNT];
+	int clear_flg;
 }	t_wordle;
 
+//utils.c
+size_t	ft_line_len(char *a_file_name);
+char **ft_set_dictionary(size_t	line_len);
+void ft_free(t_wordle *wordle);
+int ft_map_is_error(t_wordle *wordle);
 
 #endif
--- wordle/src/main.c
+++ wordle/src/main.c
@@ -6,78 +6,24 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 11:14:39 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/05/27 21:41:05 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:27:04 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wordle.h"
 
 /*
- * マップデータの行数を取得
- * 
- * @param	ファイルディスクリプタ
- * @param	ファイル名
- * @return	行数
+ * 構造体の初期化
+ *   ・辞書ファイルの行数を取得
+ *   ・辞書ファイルを文字列へ格納
+ *   ・ターミナルへ出力する文字情報を格納
  */
-size_t	ft_line_len(char *a_file_name) {
-	ssize_t	r_size;
-	size_t	r_return_num;
-	char	r_buf[1];
-	int		r_fd;
-
-	r_fd = open(a_file_name, O_RDONLY);
-	if (r_fd == -1)
-		return (0);
-	r_return_num = 1;
-	while (1)
-	{
-		r_size = read(r_fd, r_buf, 1);
-		if (r_size == -1)
-		{
-			close(r_fd);
-			return (0);
-		}
-		if (r_size == 0)
-		{
-			close(r_fd);
-			return (r_return_num);
-		}
-		if (r_buf[0] == '\n')
-			r_return_num++;
-	}
-}
-
-/*
- * マップデータを2次元配列へ格納する
- * 
- * @param	コマンドライン引数の値
- * @return	マップデータを2次元配列へ格納した値
- */
-char **ft_set_dictionary(size_t	line_len) {
-	int		r_fd;
-	char **dictionary;
-	size_t	i;
-
-	r_fd = open(FILE_NAME, O_RDONLY);
-	if (r_fd == -1)
-		return NULL;
-	dictionary = (char **)malloc(sizeof(char *) * (line_len + 1));
-	if (dictionary == NULL)
-		return NULL;
-	i = 0;
-	while (i < line_len)
-	{
-		dictionary[i] = get_next_line(r_fd);
-		i++;
-	}
-	dictionary[i] = NULL;
-	close(r_fd);
-	return(dictionary);
-}
-
 int ft_init(t_wordle *wordle) {
 	wordle->dictionary_size = ft_line_len(FILE_NAME);
 	if (wordle->dictionary_size == 0) {
+		return (0);
+	}
+	if (ft_map_is_error(wordle)) {
 		return (0);
 	}
 	for (int i = 0; i < TRY_COUNT; i++) {
@@ -92,14 +38,18 @@ int ft_init(t_wordle *wordle) {
 	}
 	srand(time(NULL));
 	wordle->target_str = wordle->dictionary[rand() % (int)wordle->dictionary_size];
+	wordle->clear_flg = 0;
 	return (1);
 }
 
+/*
+ * ターミナルにマスの情報を表示する
+ */
 void ft_put_result(t_wordle *wordle) {
 	for (int i = 0; i < TRY_COUNT; i++) {
 		for (int j = 0; j < PRINT_COUNT; j++) {
-			if (wordle->color_info[i][j] == RED_FLG)
-				printf(RED_COLOR "%c " RESET_COLOR, wordle->put_str[i][j]);
+			if (wordle->color_info[i][j] == GREEN_FLG)
+				printf(GREEN_COLOR "%c " RESET_COLOR, wordle->put_str[i][j]);
 			else if (wordle->color_info[i][j] == YELLOW_FLG)
 				printf(YELLOW_COLOR "%c " RESET_COLOR, wordle->put_str[i][j]);
 			else if (wordle->color_info[i][j] == GREY_FLG)
@@ -111,8 +61,10 @@ void ft_put_result(t_wordle *wordle) {
 	}
 }
 
+/*
+ * 入力された文字が辞書の中にあるか判断する
+ */
 int ft_is_word_error(t_wordle wordle, char *input_str) {
-	printf("size = %zu\n", wordle.dictionary_size);
 	for (size_t i = 0; i < wordle.dictionary_size; i++) {
 		if (strncmp(wordle.dictionary[i], input_str, PRINT_COUNT) == 0)
 			return (0);
@@ -120,27 +72,58 @@ int ft_is_word_error(t_wordle wordle, char *input_str) {
 	return (1);
 }
 
-// void ft_judgment(t_wordle *wordle, char *input_str) {
-// 	for (int i = 0; i < PRINT_COUNT; i++) {
-// 		for (int j = 0; j < PRINT_COUNT; j++) {
-// 			if (input_str[i] == wordle->target_str[i][j]) {
-// 				if (i == j)
-// 					wordle->color_info[i][j];
-// 			}
-// 		}
-// 	}
-// }
+/*
+ * 入力文字とターゲットが一致しているか判断する。フラグを設定する。
+ */
+void ft_judgment(t_wordle *wordle, char *input_str, int index) {
+	int jud_flg[5];
 
-//TODO:dictonaryのfree
-//		判定のロジック
-//		検証、リーク確認
+	bzero(jud_flg, sizeof(int) * 5);
+	for (int i = 0; i < PRINT_COUNT; i++) {
+		wordle->put_str[index][i] = toupper(input_str[i]);
+		wordle->color_info[index][i] = GREY_FLG;
+		for (int j = 0; j < PRINT_COUNT; j++) {
+			if (toupper(input_str[i]) == toupper(wordle->target_str[j])) {
+				if (i == j) {
+					wordle->color_info[index][i] = GREEN_FLG;
+					jud_flg[i] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < PRINT_COUNT; i++) {
+		wordle->put_str[index][i] = toupper(input_str[i]);
+		for (int j = 0; j < PRINT_COUNT; j++) {
+			if (jud_flg[j] == 0 && toupper(input_str[i]) == toupper(wordle->target_str[j])) {
+				if (wordle->color_info[index][i] != GREEN_FLG) {
+					wordle->color_info[index][i] = YELLOW_FLG;
+					jud_flg[j] = 1;
+					break;
+				}
+			}
+		}
+	}
+}
+
+/*
+ * 入力文字が全てあっているか判断する関数
+ * 全て緑なら1を返す
+ */
+int ft_is_finish(t_wordle wordle, int index) {
+	for (int i = 0; i < PRINT_COUNT; i++) {
+		if (wordle.color_info[index][i] != GREEN_FLG) {
+			return (0);
+		}
+	}
+	return (1);
+}
+
 int main(void) {
 	char *input_str;
 	t_wordle wordle;
 
 	if (ft_init(&wordle) == 0)
 		return (0);
-	printf("str = %s", wordle.target_str);
 	printf("登録されている単語は、%zu個です。\n", wordle.dictionary_size);
 	ft_put_result(&wordle);
 	for (int i = 0; i < TRY_COUNT; i++) {
@@ -148,24 +131,42 @@ int main(void) {
 		fflush(stdout);
 		input_str = get_next_line(STDIN_FILENO);
 		if (strlen(input_str) > TRY_COUNT) {
-			printf(RED_COLOR "文字数が多すぎます。\n" RESET_COLOR);
+			printf(RED_COLOR "文字数が多いです。\n" RESET_COLOR);
+			free(input_str);
+			i--;
+			continue;
+		} else if (strlen(input_str) < TRY_COUNT) {
+			printf(RED_COLOR "文字数が少ないです。\n" RESET_COLOR);
+			free(input_str);
 			i--;
 			continue;
 		} else if (ft_is_word_error(wordle, input_str)) {
 			printf(RED_COLOR "この単語は登録されていません。\n" RESET_COLOR);
+			free(input_str);
 			i--;
 			continue;
 		} else {
-			// ft_judgment(&wordle, input_str);
+			ft_judgment(&wordle, input_str, i);
 			ft_put_result(&wordle);
+			free(input_str);
+			if (ft_is_finish(wordle, i)) {
+				printf("クリア！\n");
+				wordle.clear_flg = 1;
+				fflush(stdout);
+				break;
+			}
 		}
 	}
+	if (wordle.clear_flg == 0) {
+		printf("答えは%s", wordle.target_str);
+	}
+	ft_free(&wordle);
 	return (0);
 }
 
-	// int i = 0;
-	// while (wordle.dictionary[i] != NULL) {
-	// 	printf("%s", wordle.dictionary[i]);
-	// 	i++;
-	// }
-	// printf ("\ni = %d\n", i);
+// #include <libc.h>
+
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q wordle");
+// }
